6ba354caab96bd08b2c6bd6d5577451e2fcbc153
Simen Russnes <simen.russnes@gmail.com>
2021-10-01 17:27:01 +0200

    added APIView

--- prospector_test/myapp/views.py
+++ prospector_test/myapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import status, views
+from rest_framework.response import Response
+from . import models
 
-# Create your views here.
+
+def my_func(a):
+    print(a)
+
+
+class MyView(views.APIView):
+    def get(self, request):
+        user_id = request.user
+        print(user_id)
+        all_models = models.MyModel.objects.all()
+        print(all_models)
+
+        my_func(1, 1)
+        models.doesnotexist()
+
+        return Response(data={"status": "Success!"}, status=status.HTTP_200_OK)
